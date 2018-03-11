8ced864a96ae71e8e221137a0aebc0387a13e774
Horia Popa <horia@live.com>
2018-03-11 12:00:31 +0200

    SP to import data is using dynamic queries

--- Ness_DB.sql
+++ Ness_DB.sql
@@ -606,3 +606,128 @@ End
 
 Go
 
+If Object_Id(N'dbo.uspImportDataFromTiVoRaw', N'P') Is Not Null
+    Drop Procedure dbo.uspImportDataFromTiVoRaw;
+Go
+
+Create Procedure dbo.uspImportDataFromTiVoRaw
+	@SourceTable nvarchar(50),
+	@Result int Out
+As
+SET NOCOUNT ON
+Begin
+	Declare @PersonnelId nvarchar(255)
+	Declare @ExpenditureDate datetime
+	Declare @ProjectNumber nvarchar(255)
+	Declare @TaskNumber nvarchar(255)
+	Declare @Quantity float
+	Declare @RecordCount int
+	Declare @SourceSQL nvarchar(255)
+	Declare @TiVoDataCursor Cursor
+
+	Set @Result = 0
+	Set @RecordCount = 0
+
+	Set @SourceSQL = N'Set @TiVoDataCursor = Cursor Local Read_Only Forward_Only Static For Select [Personnel Id], [Expenditure Date], [Project Number], [Task Number], [Quantity] From ' + @SourceTable;
+	Set @SourceSQL += N'; \
+	Open @TiVoDataCursor;'
+	Execute sp_executesql @SourceSQL, N'@TiVoDataCursor CURSOR OUTPUT', @TiVoDataCursor Output
+	
+	Fetch Next From @TiVoDataCursor   
+	Into @PersonnelId, @ExpenditureDate, @ProjectNumber, @TaskNumber, @Quantity
+
+	While @@FETCH_STATUS = 0  
+	Begin
+		Select @RecordCount = Count(*) From TiVo_Data 
+		Where 
+			[Contractor Number] = @PersonnelId
+			And [Time Entry Date] = @ExpenditureDate
+			And [Project Number] = @ProjectNumber
+			And [Task Number] = @TaskNumber
+			And [Worked Hours] = @Quantity
+
+		If @RecordCount > 1
+		Begin
+			Print 'Warning: For ' + @PersonnelId + ' on ' + Cast(@ExpenditureDate as varchar(50)) + ' for project ' + @ProjectNumber + ' and task ' + @TaskNumber + ' there are ' + Cast(@RecordCount as varchar(10)) + ' entries in TiVo_Data'
+		End
+
+		If @RecordCount = 0
+		Begin
+			Print 'Adding data for user ' + @PersonnelId + ' on '  + Cast(@ExpenditureDate as varchar(50)) + ' for project ' + @ProjectNumber + ' and task ' + @TaskNumber 
+
+			Insert Into TiVo_Data (
+				[Supplier Number],		-- ok
+				[Supplier Name],		-- ok
+				[Contractor Number],	-- ok
+				[Contractor Name],		-- ok
+				[Supervisor Name],		-- ok
+				[Time Entry Date],		-- ok
+				[Po Number],			-- ok
+				[Project Number],		-- ok
+				[Project Name],			-- ok
+				[Task Number],			-- ok
+				[Task Name],			-- ok
+				[Approver Name],		-- ok
+				[Approved Date],		-- ok
+				[Contractor Start Date],-- ok
+				[Contractor End Date],	-- ok
+				[Expenditure Type],		-- ok
+				[Organization Name],	-- ok
+				[Hours Type],			-- ok
+				[Worked Hours],			-- ok
+				[UOM],					-- ok
+				[Contractor Rate],		-- ok
+				[Invoice Number],		-- ok
+				[Invoice Line Num],
+				[Invoice Line Date],
+				[Total Inv Amount],
+				[Comments],
+				[Actual Worked Hours (if different from Worked Hours)])
+			Select 
+				[Vendor Number],		-- [Supplier Number]
+				[Vendor Name],			-- [Supplier Name]
+				[Personnel Id],			-- [Contractor Number]
+				[Personnel Name],		-- [Contractor Name]
+				Null,					-- [Supervisor Name]
+				[Expenditure Date],		-- [Time Entry Date]
+				[PO Number],			-- [Po Number]
+				[Project Number],		-- [Project Number]
+				[Project Name],			-- [Project Name]
+				[Task Number],			-- [Task Number]
+				[Task Name],			-- [Task Name]
+				Null,					-- [Approver Name]
+				Null,					-- [Approved Date]
+				Null,					-- [Contractor Start Date]
+				Null,					-- [Contractor End Date]
+				[Expenditure Type],		-- [Expenditure Type]
+				[Project Organization],	-- [Organization Name]
+				Null,					-- [Hours Type]
+				[Quantity],				-- [Worked Hours]
+				[UOM],					-- [UOM]
+				Null,					-- [Contractor Rate]
+				[Invoice Number],		-- [Invoice Number]
+				Null, -- Invoice line num
+				Null, -- Invoice line date
+				Null, -- total invoice amount
+				Null, -- comments
+				Null -- Actual worked hours
+			From 
+				TiVo_Data_Raw
+			Where
+				[Personnel Id] = @PersonnelId
+				And [Expenditure Date] = @ExpenditureDate
+				And [Project Number] = @ProjectNumber
+				And [Task Number] = @TaskNumber
+				And [Quantity] = @Quantity
+		End
+
+		Fetch Next From @TiVoDataCursor   
+		Into @PersonnelId, @ExpenditureDate, @ProjectNumber, @TaskNumber, @Quantity
+	End
+
+	Close @TiVoDataCursor
+	Deallocate @TiVoDataCursor
+
+End
+
+Go
